696422b72349f186f980c8477152bfd32ae0cfd8
Иван Васильев <52786880+0ceiros@users.noreply.github.com>
2020-08-26 17:14:29 +0300

    Убрал вывод дебага при отправке писем

--- src/main/java/g_server/g_server/application/service/mail/MailConfig.java
+++ src/main/java/g_server/g_server/application/service/mail/MailConfig.java
@@ -29,7 +29,6 @@ public class MailConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
 
         return mailSender;
     }
