9d707efbc83f65c3655bc067c437a8e494c505a5
Alamart1683 <Alamart1683@yandex.ru>
2020-08-29 09:46:20 +0300

    Мелкие правки

--- src/main/java/g_server/g_server/application/controller/documents/DocumentDownloadController.java
+++ src/main/java/g_server/g_server/application/controller/documents/DocumentDownloadController.java
@@ -5,17 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-import java.net.http.HttpRequest;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class DocumentDownloadController {
@@ -31,7 +27,7 @@ public class DocumentDownloadController {
     ) {
         // TODO Сделать присвоение файлу адекватного имени
         // TODO Разобраться как вытащить тоек из HTTP запроса напрямую
-        // httpServletRequest.getAuthType();
+        // TODO Убрать костыли в скачивании документов, сделать её для каждой из версий
         File file = documentDownloadService.findDownloadDocument(documentName, token);
         String contentType = documentDownloadService.getContentType(file.getName());
         String extension = documentDownloadService.getFileExtension(file.getName());
