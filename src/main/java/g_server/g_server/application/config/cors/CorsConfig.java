f67b2b19bc0f5e6cc3c77595c80ca53a3d06d020
Иван Васильев <52786880+0ceiros@users.noreply.github.com>
2020-10-04 17:11:42 +0300

    Добавил конфигурацию CORS
    Т.е. настройка возможности получать запросы с других адресов, с адреса фронта в частности

---
+++ src/main/java/g_server/g_server/application/config/cors/CorsConfig.java
@@ -0,0 +1,23 @@
+package g_server.g_server.application.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class  CorsConfig implements WebMvcConfigurer {
+
+    final String webFrontURL = "http://localhost:3000";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(webFrontURL)
+            .allowCredentials(true)
+            .allowedHeaders("*")
+            .allowedMethods("*");
+    }
+
+}
