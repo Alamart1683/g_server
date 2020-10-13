a5f7a21b3551d35761eb32813c60e9b837d37b2e
Alamart1683 <Alamart1683@yandex.ru>
2020-10-13 21:47:19 +0300

    Работает замена кроме таблиц

--- src/main/java/g_server/g_server/application/service/documents/DocumentProcessorService.java
+++ src/main/java/g_server/g_server/application/service/documents/DocumentProcessorService.java
@@ -1,6 +1,5 @@
 package g_server.g_server.application.service.documents;
 
-import com.aspose.words.FindReplaceOptions;
 import g_server.g_server.application.config.jwt.JwtProvider;
 import g_server.g_server.application.entity.documents.Document;
 import g_server.g_server.application.entity.documents.DocumentVersion;
@@ -13,12 +12,21 @@ import g_server.g_server.application.repository.documents.DocumentVersionReposit
 import g_server.g_server.application.repository.system_data.SpecialityRepository;
 import g_server.g_server.application.repository.users.AssociatedStudentsRepository;
 import g_server.g_server.application.repository.users.UsersRepository;
+import g_server.g_server.application.service.documents.TextReplace.DocxTextReplaceService;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.io.File;
+
+import java.io.*;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.regex.Pattern;
 
 @Service
 public class DocumentProcessorService {
@@ -74,63 +82,145 @@ public class DocumentProcessorService {
                     }
                     Document currentTask = taskList.get(taskList.size() - 1);
                     List<DocumentVersion> taskVersions = documentVersionRepository.findByDocument(currentTask.getId());
-                    com.aspose.words.Document template =
-                            new com.aspose.words.Document(taskVersions.get(taskVersions.size() - 1)
-                            .getThis_version_document_path()
-                    );
-                    FindReplaceOptions options = new FindReplaceOptions();
+                    DocumentVersion taskVersion = taskVersions.get(taskVersions.size() - 1);
+                    XWPFDocument template = openDocument(taskVersion.getThis_version_document_path());
+
                     // Заменим тему проекта на полученную
-                    template.getRange().replace(Pattern.compile("Согласованное название темы"), taskDataView.getStudentTheme(), options);
+                    // docxTextReplaceService.setSearchValue("Согласованное название темы");
+                    // docxTextReplaceService.setReplacement(taskDataView.getStudentTheme());
+                    // docxTextReplaceService.replace(template);
+
+                    replace(template, "Согласованное название темы", taskDataView.getStudentTheme());
 
                     // Заменим дату выхода приказа вида «XX» месяца YYYY
-                    template.getRange().replace(Pattern.compile("o«XX» месяца YYYY"), getFirstDateType(taskDataView.getOrderDate()), options);
+                    //docxTextReplaceService.setSearchValue("o«XX» месяца YYYY");
+                    //docxTextReplaceService.setReplacement(getFirstDateType(taskDataView.getOrderDate()));
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "o«XX» месяца YYYY", getFirstDateType(taskDataView.getOrderDate()));
+
                     // Заменим дату начала НИРа вида «XX» месяца YYYY
-                    template.getRange().replace(Pattern.compile("s«XX» месяца YYYY"), getFirstDateType(taskDataView.getOrderStartDate()), options);
+                    //docxTextReplaceService.setSearchValue("s«XX» месяца YYYY");
+                    //docxTextReplaceService.setReplacement(getFirstDateType(taskDataView.getOrderStartDate()));
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "b«XX» месяца YYYY", getFirstDateType(taskDataView.getOrderStartDate()));
+
                     // Заменим дату окончания НИРа вида «XX» месяца YYYY
-                    template.getRange().replace(Pattern.compile("e«XX» месяца YYYY"), getFirstDateType(taskDataView.getOrderStartDate()), options);
+                    //docxTextReplaceService.setSearchValue("e«XX» месяца YYYY");
+                    //docxTextReplaceService.setReplacement(getFirstDateType(taskDataView.getOrderEndDate()));
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "e«XX» месяца YYYY", getFirstDateType(taskDataView.getOrderEndDate()));
+
                     // Заменим дату начала НИРа вида XX месяца YYYY
-                    template.getRange().replace(Pattern.compile("sXX месяца YYYY"), getSecondDateType(taskDataView.getOrderStartDate()), options);
+                    //docxTextReplaceService.setSearchValue("sXX месяца YYYY");
+                    //docxTextReplaceService.setReplacement(getFirstDateType(taskDataView.getOrderStartDate()));
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "bXX месяца YYYY", getFirstDateType(taskDataView.getOrderStartDate()));
+
                     // Заменим дату окончания НИРа вида XX месяца YYYY
-                    template.getRange().replace(Pattern.compile("eXX месяца YYYY"), getSecondDateType(taskDataView.getOrderEndDate()), options);
+                    //docxTextReplaceService.setSearchValue("eXX месяца YYYY");
+                    //docxTextReplaceService.setReplacement(taskDataView.getOrderEndDate());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "eXX месяца YYYY", taskDataView.getOrderEndDate());
+
                     // Заменим дату начала НИРа вида XX.YY.ZZZZ
-                    template.getRange().replace(Pattern.compile("sXX\\.YY\\.ZZ"), taskDataView.getOrderStartDate(), options);
+                    //docxTextReplaceService.setSearchValue("sXX.YY.ZZ");
+                    //docxTextReplaceService.setReplacement(taskDataView.getOrderStartDate());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "bXX.YY.ZZ", taskDataView.getOrderStartDate());
+
                     // Заменим дату окончания НИРа вида XX.YY.ZZZZ
-                    template.getRange().replace(Pattern.compile("eXX\\.YY\\.ZZ"), taskDataView.getOrderStartDate(), options);
+                    //docxTextReplaceService.setSearchValue("eXX.YY.ZZ");
+                    //docxTextReplaceService.setReplacement(taskDataView.getOrderEndDate());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "eXX.YY.ZZ", taskDataView.getOrderEndDate());
 
                     // Заменим номер приказа на полученный
-                    template.getRange().replace(Pattern.compile("Номер приказа"), taskDataView.getOrderNumber(), options);
+                    //docxTextReplaceService.setSearchValue("Номер приказа");
+                    //docxTextReplaceService.setReplacement( taskDataView.getOrderNumber());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "Номер приказа", taskDataView.getOrderNumber());
 
                     // Заменим название кафедры на полученное
-                    template.getRange().replace(Pattern.compile("sКАФЕДРА"), taskDataView.getCathedra(), options);
+                    //docxTextReplaceService.setSearchValue("sКАФЕДРА");
+                    //docxTextReplaceService.setReplacement(taskDataView.getCathedra());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "nКАФЕДРА", taskDataView.getCathedra());
 
                     // Заменим группу студента на полученную
-                    template.getRange().replace(Pattern.compile("gXXXX-YY-ZZ"), taskDataView.getStudentGroup(), options);
+                    //docxTextReplaceService.setSearchValue("gXXXX-YY-ZZ");
+                    //docxTextReplaceService.setReplacement(taskDataView.getStudentGroup());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "XXXX-YY-ZZ", taskDataView.getStudentGroup());
 
                     // Заменим код специальности
-                    template.getRange().replace(Pattern.compile("cКод специальности"), taskDataView.getOrderSpeciality(), options);
+                    //docxTextReplaceService.setSearchValue("cКод специальности");
+                    //docxTextReplaceService.setReplacement(taskDataView.getOrderSpeciality());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "cКод специальности", taskDataView.getOrderSpeciality());
 
                     // Заменим название специальности
                     Speciality speciality;
                     try {
-                       speciality = specialityRepository.findByCode(taskDataView.getOrderSpeciality());
+                        speciality = specialityRepository.findByCode(taskDataView.getOrderSpeciality());
                     } catch (NullPointerException nullPointerException){
                         speciality = null;
                     }
                     if (speciality != null) {
                         // Заменим название специальности
-                        template.getRange().replace(Pattern.compile("sНазвание специальности"), speciality.getSpeciality(), options);
+                        //docxTextReplaceService.setSearchValue("sНазвание специальности");
+                        //docxTextReplaceService.setReplacement(speciality.getSpeciality());
+                        //docxTextReplaceService.replace(template);
+
+                        replace(template, "nНазвание специальности", speciality.getSpeciality());
                     }
 
                     // Заменим ФИО студента на укороченную версию вида Иванов И.И.
-                    template.getRange().replace(Pattern.compile("ФИО Студента"), getShortFio(taskDataView.getStudentFio()), options);
-                    // Заменим ФИО руководителя на укороченную версию вида Иванов И.И.
-                    template.getRange().replace(Pattern.compile("ФИО Руководителя"), getShortFio(taskDataView.getAdvisorFio()), options);
-                    // Заменим ФИО завкафа на укороченную версию вида Иванов И.И.
-                    template.getRange().replace(Pattern.compile("ФИО Зав\\. кафедрой"), getShortFio(taskDataView.getHeadFio()), options);
-                    // Заменим ФИО студента на фио в дательном падеже
-                    template.getRange().replace(Pattern.compile("Полное ФИО студента в Д\\.П\\."), getShortFio(taskDataView.getStudentFio()), options);
+                    //docxTextReplaceService.setSearchValue("ФИО Студента");
+                    //docxTextReplaceService.setReplacement( getShortFio(taskDataView.getStudentFio()));
+                    //docxTextReplaceService.replace(template);
 
-                    template.save(studentDocumentsPath + File.separator + "temp.docx");
+                    replace(template, "ФИОСтудента", getShortFio(taskDataView.getStudentFio()));
+
+                    // Заменим ФИО руководителя на укороченную версию вида Иванов И.И.
+                    //docxTextReplaceService.setSearchValue("ФИО Руководителя");
+                    //docxTextReplaceService.setReplacement(getShortFio(taskDataView.getAdvisorFio()));
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "ФИОРуководителя", getShortFio(taskDataView.getAdvisorFio()));
+
+                    // Заменим ФИО завкафа на укороченную версию вида Иванов И.И.
+                    //docxTextReplaceService.setSearchValue("ФИО Зав. кафедрой");
+                    //docxTextReplaceService.setReplacement(getShortFio(taskDataView.getHeadFio()));
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "ФИОЗавкафедрой", getShortFio(taskDataView.getHeadFio()));
+
+                    // Заменим ФИО студента на фио в дательном падеже
+                    //docxTextReplaceService.setSearchValue("Полное ФИО студента в Д.П.");
+                    //docxTextReplaceService.setReplacement(taskDataView.getStudentFio());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "Полное ФИО студента в Д.П.", taskDataView.getStudentFio());
+
+                    // Заменим ФИО студента на фио в родительном падеже
+                    //docxTextReplaceService.setSearchValue("Полное ФИО студента в Р.П.");
+                    //docxTextReplaceService.setReplacement(taskDataView.getStudentFio());
+                    //docxTextReplaceService.replace(template);
+
+                    replace(template, "Полное ФИО студента в Р.П.", taskDataView.getStudentFio());
+
+                    saveDocument(template, studentDocumentsPath + File.separator + "temp.docx");
                     File file = new File(studentDocumentsPath + File.separator + "temp.docx");
                     return file;
                 } else {
@@ -186,39 +276,150 @@ public class DocumentProcessorService {
     // Получить слово месяца
     public String getMonthWord(String month) {
         // Определим месяц
-        String monthWord;
+        String monthWord = "";
         switch (month) {
             case "01":
                 monthWord = "января";
+                return monthWord;
             case "02":
                 monthWord = "февраля";
+                return monthWord;
             case "03":
                 monthWord = "марта";
+                return monthWord;
             case "04":
                 monthWord = "апреля";
+                return monthWord;
             case "05":
                 monthWord = "мая";
+                return monthWord;
             case "06":
                 monthWord = "июня";
+                return monthWord;
             case "07":
                 monthWord = "июля";
+                return monthWord;
             case "08":
                 monthWord = "августа";
+                return monthWord;
             case "09":
                 monthWord = "сентября";
+                return monthWord;
             case "10":
                 monthWord = "октября";
+                return monthWord;
             case "11":
                 monthWord = "ноября";
+                return monthWord;
             case "12":
                 monthWord = "декабря";
+                return monthWord;
             default:
                 monthWord = "ошибка";
+                return monthWord;
         }
-        return monthWord;
     }
 
-    // Необходимо получить id пользователя-создателя документа из токена
+    private static Map<Integer, XWPFRun> getPosToRuns(XWPFParagraph paragraph) {
+        int pos = 0;
+        Map<Integer, XWPFRun> map = new HashMap<Integer, XWPFRun>(10);
+        for (XWPFRun run : paragraph.getRuns()) {
+            String runText = run.text();
+            if (runText != null) {
+                for (int i = 0; i < runText.length(); i++) {
+                    map.put(pos + i, run);
+                }
+                pos += runText.length();
+            }
+        }
+        return (map);
+    }
+
+    public static <V> void replace(XWPFDocument document, Map<String, V> map) {
+        List<XWPFParagraph> paragraphs = document.getParagraphs();
+        for (XWPFParagraph paragraph : paragraphs) {
+            replace(paragraph, map);
+        }
+    }
+
+    public static <V> void replace(XWPFDocument document, String searchText, V replacement) {
+        List<XWPFParagraph> paragraphs = document.getParagraphs();
+        for (XWPFParagraph paragraph : paragraphs) {
+            replace(paragraph, searchText, replacement);
+        }
+    }
+
+    private static <V> void replace(XWPFParagraph paragraph, Map<String, V> map) {
+        for (Map.Entry<String, V> entry : map.entrySet()) {
+            replace(paragraph, entry.getKey(), entry.getValue());
+        }
+    }
+
+    public static <V> void replace(XWPFParagraph paragraph, String searchText, V replacement) {
+        boolean found = true;
+        while (found) {
+            found = false;
+            int pos = paragraph.getText().indexOf(searchText);
+            if (pos >= 0) {
+                found = true;
+                Map<Integer, XWPFRun> posToRuns = getPosToRuns(paragraph);
+                XWPFRun run = posToRuns.get(pos);
+                XWPFRun lastRun = posToRuns.get(pos + searchText.length() - 1);
+                int runNum = paragraph.getRuns().indexOf(run);
+                int lastRunNum = paragraph.getRuns().indexOf(lastRun);
+                String texts[] = replacement.toString().split("\n");
+                run.setText(texts[0], 0);
+                XWPFRun newRun = run;
+                for (int i = 1; i < texts.length; i++) {
+                    newRun.addCarriageReturn();
+                    newRun = paragraph.insertNewRun(runNum + i);
+                /*
+                    We should copy all style attributes
+                    to the newRun from run
+                    also from background color, ...
+                    Here we duplicate only the simple attributes...
+                 */
+                    newRun.setText(texts[i]);
+                    newRun.setBold(run.isBold());
+                    newRun.setCapitalized(run.isCapitalized());
+                    // newRun.setCharacterSpacing(run.getCharacterSpacing());
+                    newRun.setColor(run.getColor());
+                    newRun.setDoubleStrikethrough(run.isDoubleStrikeThrough());
+                    newRun.setEmbossed(run.isEmbossed());
+                    newRun.setFontFamily(run.getFontFamily());
+                    newRun.setFontSize(run.getFontSize());
+                    newRun.setImprinted(run.isImprinted());
+                    newRun.setItalic(run.isItalic());
+                    newRun.setKerning(run.getKerning());
+                    newRun.setShadow(run.isShadowed());
+                    newRun.setSmallCaps(run.isSmallCaps());
+                    newRun.setStrikeThrough(run.isStrikeThrough());
+                    newRun.setSubscript(run.getSubscript());
+                    newRun.setUnderline(run.getUnderline());
+                }
+                for (int i = lastRunNum + texts.length - 1; i > runNum + texts.length - 1; i--) {
+                    paragraph.removeRun(i);
+                }
+            }
+        }
+    }
+
+    public XWPFDocument openDocument(String file) throws Exception {
+        XWPFDocument document = null;
+        InputStream is = new FileInputStream(file);
+        document = new XWPFDocument(is);
+        return document;
+    }
+
+    // Сохранить документ
+    public void saveDocument(XWPFDocument doc, String file) {
+        try (FileOutputStream out = new FileOutputStream(file)) {
+            doc.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public Integer getUserId(String token) {
         String email = jwtProvider.getEmailFromToken(token);
         Users user = usersRepository.findByEmail(email);
---
+++ src/main/java/g_server/g_server/application/service/documents/TextReplace/DocTextReplaceService.java
@@ -0,0 +1,52 @@
+package g_server.g_server.application.service.documents.TextReplace;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.usermodel.CharacterRun;
+import org.apache.poi.hwpf.usermodel.Paragraph;
+import org.apache.poi.hwpf.usermodel.Range;
+import org.apache.poi.hwpf.usermodel.Section;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class DocTextReplaceService {
+    // Заменить слово в документе
+    public HWPFDocument replaceText(HWPFDocument doc, String findText, String replaceText) {
+        Range r = doc.getRange();
+        for (int i = 0; i < r.numSections(); ++i) {
+            Section s = r.getSection(i);
+            for (int j = 0; j < s.numParagraphs(); j++) {
+                Paragraph p = s.getParagraph(j);
+                for (int k = 0; k < p.numCharacterRuns(); k++) {
+                    CharacterRun run = p.getCharacterRun(k);
+                    String text = run.text();
+                    if (text.contains(findText)) {
+                        run.replaceText(findText, replaceText);
+                    }
+                }
+            }
+        }
+        return doc;
+    }
+
+    // Открыть документ
+    public HWPFDocument openDocument(String file) throws Exception {
+        URL res = getClass().getClassLoader().getResource(file);
+        HWPFDocument document = null;
+        if (res != null) {
+            document = new HWPFDocument(new POIFSFileSystem(
+                    new File(res.getPath())));
+        }
+        return document;
+    }
+
+    // Сохранить документ
+    public void saveDocument(HWPFDocument doc, String file) {
+        try (FileOutputStream out = new FileOutputStream(file)) {
+            doc.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
