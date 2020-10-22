6bae0690bc684c2717aa70397ae1bab62788bd48
Иван Васильев <52786880+0ceiros@users.noreply.github.com>
2020-10-22 22:38:50 +0300

    Очередная донастройка КОРС

--- src/main/java/g_server/g_server/application/Application.java
+++ src/main/java/g_server/g_server/application/Application.java
@@ -18,7 +18,9 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
