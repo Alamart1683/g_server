8ce22edd4145873fca31c129d198102ccc218519
Alamart1683 <Alamart1683@yandex.ru>
2020-12-06 20:09:48 +0300

    Снова починил

--- src/main/java/g_server/g_server/application/config/security/ResourcesConfig.java
+++ src/main/java/g_server/g_server/application/config/security/ResourcesConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 
+/*
 @Configuration
 public class ResourcesConfig implements WebMvcConfigurer {
     @Value("${storage.location}")
@@ -16,6 +17,7 @@ public class ResourcesConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/document/outer/view/**").addResourceLocations(file.getAbsolutePath() + File.separator);
+        registry.addResourceHandler("/document/outer/view/**").addResourceLocations(storageLocation + File.separator);
     }
 }
+ */
